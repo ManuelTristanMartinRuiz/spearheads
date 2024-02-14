b339716620b25637cc73a6d19b04e2767fc83f02
ManuelTristanMartinRuiz <manueltristanmartin@gmail.com>
2024-02-14 12:15:31 +0100

    Update spearheads.R

--- spearheads.R
+++ spearheads.R
@@ -1,12 +1,18 @@
-install.package("readxl")
 library(readxl)
-file.choose("C:/Users/manueltristan/Documents/spearheads/spearheads.xlsx")
-ruta_excel <-"C:/Users/manueltristan/Documents/spearheads/spearheads.xlsx"
-excel_sheets(ruta_excel)
-read_excel(ruta_excel)
-
-spear <- as.data.frame(ruta_excel)
-print(spear)
+#Ej.1
+spear <- read_excel("C:/Users/manueltristan/Documents/spearheads/spearheads.xlsx")
 View(spear)
+str(spear)
+class(spear)
 
-f
+spear <- as.data.frame(spear)
+class(spear)
+
+#Ej.2
+names(spear)[names(spear) == "Mat"] <- "Materiales"
+names(spear)[names(spear) == "Con"] <- "Contexto"
+names(spear)[names(spear) == "Cond"] <- "Conservacion"
+names(spear)[names(spear) == "Peg"] <- "Remache"
+names(spear)[names(spear) == "Maxle"] <- "Longitud_max"
+spear
+View(spear)
