605ea0b2f7f6b40384d26d887e772a0588c284b4
ManuelTristanMartinRuiz <manueltristanmartin@gmail.com>
2024-02-18 21:36:49 +0100

    Update spearheads.R

--- spearheads.R
+++ spearheads.R
@@ -21,6 +21,9 @@ names(spear)[names(spear) == "Date"] <- "Fecha"
 names(spear)[names(spear) == "Maxle"] <- "Longitud_max"
 names(spear)[names(spear) == "Socle"] <- "Longitud_encaje"
 names(spear)[names(spear) == "Maxwi"] <- "Ancho_max"
+names(spear)[names(spear) == "Upsoc"] <- "Ancho_encaje"
+names(spear)[names(spear) == "Mawit"] <- "Ancho_max_encaje"
+names(spear)[names(spear) == "Weight"] <- "Peso"
 spear
 View(spear)
 #Para renombrar utilizamos la función "names" y con la función lógica "==" compara con el data.frame y le asignamos nuevos nombres
@@ -45,7 +48,7 @@ View(freq.cond)
 #Ej.5
 materiales_contexto <- table(spear$Materiales, spear$Contexto)
 View(materiales_contexto)
-materiales_conservacion <-table(spear$Materiales, spear$Conservación)
+materiales_conservacion <-table(spear$Materiales, spear$Conservacion)
 View(materiales_conservacion)
 #En este ejercicio empleamos la misma función "table" que en el anterior para generar una tabla de datos, pero ahora incluimos dos columnas de datos mediante la función "$" en vez de solo una. 
 
@@ -54,19 +57,19 @@ procentaje_materiales <- prop.table(table(spear$Materiales)) * 100
 View(procentaje_materiales)
 procentaje_contexto <- prop.table(table(spear$Contexto)) * 100
 View(procentaje_contexto)
-procentaje_conservacion <- prop.table(table(spear$Conservación)) * 100
+procentaje_conservacion <- prop.table(table(spear$Conservacion)) * 100
 View(procentaje_conservacion)
 #Para este ejercicio empleamos la función "prop.table", seleccionando los datos mediante la función "$", y multiplicando por *100 para que aparezca el porcentaje. 
 
 #Ej.7
 porcentaje_materiales_contexto <- prop.table(table(spear$Materiales, spear$Contexto), margin = 1) * 100
 View(porcentaje_materiales_contexto)
-porcentaje_materiales_conservacion <- prop.table(table(spear$Materiales, spear$Conservación), margin = 1) * 100
+porcentaje_materiales_conservacion <- prop.table(table(spear$Materiales, spear$Conservacion), margin = 1) * 100
 View(porcentaje_materiales_conservacion)
 #En este ejercicio tambíen empleamos la función "prop.table" para hacer una tabla en porcentaje, incluyendo dos columnas de datos mediante el uso de la función "$". La función "margin" sirve para especificar la dimensión en la que queremos hacer el calculo. Margin = 1 significa que el porcentaje se calcule por filas.
 
 #Ej.8
-barras_verticales_conservacion <- barplot(table(spear$Conservación), main = "Frecuencia de Conservación", 
+barras_verticales_conservacion <- barplot(table(spear$Conservacion), main = "Frecuencia de Conservación", 
                                           xlab = "Conservación")
 barras_verticales_contexto <- barplot(table(spear$Contexto), main = "Frecuencia de Contexto", 
         xlab = "Contexto")
@@ -75,17 +78,18 @@ barras_verticales_contexto <- barplot(table(spear$Contexto), main = "Frecuencia 
 #Ej.9
 barras_horizontales_materiales <- barplot(table(spear$Materiales), horiz = TRUE, main = "Frecuencia de Materiales", ylab = "Materiales")
 barras_horizontales_remache <- barplot(table(spear$Remache), horiz = TRUE, main = "Frecuencia de Remache", ylab = "Remache")
-#Para elaborar estos graficos utilizamos la misma función que en Ej.8 pero añadimos la función "horiz" = True para que los haga orizontales.
+#Para elaborar estos graficos utilizamos la misma función que en Ej.8 pero añadimos la función "horiz" = True para que los haga horizontales.
 
 #Ej.10
-barras_material_conservacion <- barplot(table(spear$Conservación, spear$Materiales), beside = TRUE, legend = TRUE, main = "Conservación por Material", xlab = "Conservación", col = c("red", "blue"), names.arg = c("Excelente", "Bueno", "Regular", "Malo"))
+barras_material_conservacion <- barplot(table(spear$Conservacion, spear$Materiales), beside = TRUE, legend = TRUE, main = "Conservación por Material", xlab = "Conservación", col = c("lightblue", "blue"), names.arg = c("Excelente", "Bueno", "Regular", "Malo"))
 #En este caso empleamos la misma función de "barplot", seleccionamos los dos datos que queremos agrupar, e incluimos una leyenda con los valores que les hemos dado a las diferentes categorías de conservación
 
 #Ej.11
-sectores_conservacion <- pie(table(spear$Conservación), main = "Porcentaje de Conservación")
+sectores_conservacion <- pie(table(spear$Conservacion), main = "Porcentaje de Conservación")
 #En este caso para generar un grafico de sectores usamos la función "pie", con los datos de conservación. 
 
 #Ej.12 
 hist(spear$Longitud_max, prob = TRUE, main = "Histograma de Probabilidad de Longitud Max", xlab = "Longitud Max")
 hist(spear$Ancho_max, prob = TRUE, main = "Histograma de Probabilidad de Ancho Max", xlab = "Ancho Max")
 #En el último ejercicio empleamos la función "hist" para generar un histiograma, con dos variables continuas como es el ancho maximo y la longitud máxima que son variables continuas. 
+
